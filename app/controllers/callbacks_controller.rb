b2bad3ad8a51687a52801a316f8e4406b0b1c6db
Matthew Stevenson <stevensonmt@gmail.com>
2017-01-30 22:33:04 -0800

    first commit, install/config devise -- todo: oauth troubleshoot

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,17 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+    def github
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+
+    def google
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+
+    def facebook
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+
+end
